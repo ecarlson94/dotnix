2069e4615d32ebc02d05cf6912c492219f5956e2
Eric Carlson <e.carlson94@gmail.com>
2024-02-05 06:51:24 +0000

    add zsh options

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -23,6 +23,9 @@ in {
       autocd = true;
 
       initExtra = ''
+# =============================================================================
+#                               Plugin Overrides
+# =============================================================================
 # "zsh-users/zsh-history-substring-search"
 zmodload zsh/terminfo
 bindkey "$terminfo[kcuu1]" history-substring-search-up
@@ -69,6 +72,35 @@ ZSH_PLUGINS_ALIAS_TIPS_FORCE=1
 
 # ZSH_PLUGINS_ALIAS_TIPS_REVEAL=1
 ZSH_PLUGINS_ALIAS_TIPS_REVEAL_EXCLUDES="(_ ll vi)"
+
+# =============================================================================
+#                                   Options
+# =============================================================================
+setopt append_history           # Dont overwrite history
+setopt auto_list
+setopt auto_menu
+setopt auto_pushd
+setopt extended_history         # Also record time and duration of commands.
+setopt hist_expire_dups_first   # Clear duplicates when trimming internal hist.
+setopt hist_find_no_dups        # Dont display duplicates during searches.
+setopt hist_ignore_dups         # Ignore consecutive duplicates.
+setopt hist_ignore_all_dups     # Remember only one unique copy of the command.
+setopt hist_reduce_blanks       # Remove superfluous blanks.
+setopt hist_save_no_dups        # Omit older commands in favor of newer ones.
+setopt hist_ignore_space        # Ignore commands that start with space.
+setopt inc_append_history
+setopt interactive_comments
+setopt no_beep
+setopt no_hist_beep
+setopt no_list_beep
+setopt magic_equal_subst
+setopt notify
+setopt print_eight_bit
+setopt print_exit_value
+setopt prompt_subst
+setopt pushd_ignore_dups
+setopt share_history            # Share history between multiple shells
+setopt transient_rprompt
       '';
 
       zplug = {
