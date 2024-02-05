2c63c273436b82fac62c727829599a3557fbcdca
Eric Carlson <e.carlson94@gmail.com>
2024-02-05 07:36:03 +0000

    move zsh history configuration to nix

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -19,6 +19,20 @@ in {
       enable = true;
       enableCompletion = true;
       enableAutosuggestions = true;
+      autocd = true;
+
+      history = {
+        expireDuplicatesFirst = true;
+        extended = true;
+        ignoreDups = true;
+        ignoreAllDups = true;
+        ignoreSpace = true;
+      };
+
+      historySubstringSearch = {
+        enable = true;
+      };
+
       syntaxHighlighting = {
         enable = true;
         highlighters = [
@@ -53,18 +67,14 @@ in {
           assign = "none";
         };
       };
-      autocd = true;
 
       initExtra = ''
 # =============================================================================
 #                               Plugin Overrides
 # =============================================================================
-# "zsh-users/zsh-history-substring-search"
-zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
-bindkey "^[[1;5A" history-substring-search-up
-bindkey "^[[1;5B" history-substring-search-down
+
+ZSH_HIGHLIGHT_PATTERNS+=('rm -rf *' 'fg=white,bold,bg=red')
+ZSH_HIGHLIGHT_PATTERNS+=('rmf *' 'fg=white,bold,bg=red')
 
 # "zsh-users/zsh-autosuggestions"
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=075'
@@ -83,14 +93,6 @@ setopt append_history           # Dont overwrite history
 setopt auto_list
 setopt auto_menu
 setopt auto_pushd
-setopt extended_history         # Also record time and duration of commands.
-setopt hist_expire_dups_first   # Clear duplicates when trimming internal hist.
-setopt hist_find_no_dups        # Dont display duplicates during searches.
-setopt hist_ignore_dups         # Ignore consecutive duplicates.
-setopt hist_ignore_all_dups     # Remember only one unique copy of the command.
-setopt hist_reduce_blanks       # Remove superfluous blanks.
-setopt hist_save_no_dups        # Omit older commands in favor of newer ones.
-setopt hist_ignore_space        # Ignore commands that start with space.
 setopt inc_append_history
 setopt interactive_comments
 setopt no_beep
@@ -102,18 +104,15 @@ setopt print_eight_bit
 setopt print_exit_value
 setopt prompt_subst
 setopt pushd_ignore_dups
-setopt share_history            # Share history between multiple shells
 setopt transient_rprompt
       '';
 
       zplug = {
         enable = true;
         plugins = [
-          { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
           { name = "jeffreytse/zsh-vi-mode"; }
           { name = "djui/alias-tips"; }
           { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
-          { name = "zsh-users/zsh-history-substring-search"; tags = [ as:plugin ]; }
           { name = "bric3/nice-exit-code"; }
           { name = "chrissicool/zsh-256color"; }
           { name = "plugins/git"; tags = [ from:oh-my-zsh ]; }
