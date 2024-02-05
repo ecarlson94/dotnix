0af0e5905d76ae1cf2883b3d23b6d6d7fea0ac7e
Eric Carlson <e.carlson94@gmail.com>
2024-02-05 06:47:27 +0000

    remove unnecessary zsh plugins, add color options

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -20,9 +20,55 @@ in {
       enableCompletion = true;
       enableAutosuggestions = true;
       syntaxHighlighting.enable = true;
+      autocd = true;
 
       initExtra = ''
-        bindkey "''${key[Up]}" up-line-or-search
+# "zsh-users/zsh-history-substring-search"
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey "^[[1;5A" history-substring-search-up
+bindkey "^[[1;5B" history-substring-search-down
+
+# "zsh-users/zsh-syntax-highlighting"
+typeset -gA ZSH_HIGHLIGHT_STYLES ZSH_HIGHLIGHT_PATTERNS
+
+ZSH_HIGHLIGHT_STYLES[cursor]=fg=yellow,bold
+ZSH_HIGHLIGHT_STYLES[default]=none
+ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=green,bold
+ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=green,bold
+ZSH_HIGHLIGHT_STYLES[alias]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[builtin]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[function]=fg=cyan,bold
+ZSH_HIGHLIGHT_STYLES[command]=fg=white,bold
+ZSH_HIGHLIGHT_STYLES[precommand]=fg=white,underline
+ZSH_HIGHLIGHT_STYLES[commandseparator]=none
+ZSH_HIGHLIGHT_STYLES[hashed-command]=fg=green,bold
+ZSH_HIGHLIGHT_STYLES[path]=fg=214,underline
+ZSH_HIGHLIGHT_STYLES[globbing]=fg=063
+ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=white,underline
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=070
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=070
+ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=none
+ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=063
+ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=063
+ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=009
+ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=009
+ZSH_HIGHLIGHT_STYLES[assign]=none
+
+ZSH_HIGHLIGHT_PATTERNS=('rm -rf *' 'fg=white,bold,bg=red')
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor line)
+
+# "zsh-users/zsh-autosuggestions"
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=075'
+
+# "djui/alias-tips"
+ZSH_PLUGINS_ALIAS_TIPS_EXCLUDES="(_ ll vi)"
+ZSH_PLUGINS_ALIAS_TIPS_FORCE=1
+
+# ZSH_PLUGINS_ALIAS_TIPS_REVEAL=1
+ZSH_PLUGINS_ALIAS_TIPS_REVEAL_EXCLUDES="(_ ll vi)"
       '';
 
       zplug = {
@@ -33,10 +79,10 @@ in {
           { name = "djui/alias-tips"; }
           { name = "hlissner/zsh-autopair"; tags = [ defer:2 ]; }
           { name = "zsh-users/zsh-history-substring-search"; tags = [ as:plugin ]; }
-          { name = "b4b4r07/zsh-history-ltsv"; }
           { name = "bric3/nice-exit-code"; }
           { name = "chrissicool/zsh-256color"; }
           { name = "plugins/git"; tags = [ from:oh-my-zsh ]; }
+
         ];
       };
 
