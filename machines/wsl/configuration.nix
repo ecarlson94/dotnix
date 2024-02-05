3a9e229d4665e04bda0a7e64bedee1d110182764
Eric Carlson <e.carlson94@gmail.com>
2024-02-05 05:33:31 +0000

    move wsl rebuild to bin folder

--- machines/wsl/configuration.nix
+++ machines/wsl/configuration.nix
@@ -30,7 +30,7 @@
   environment.systemPackages = with pkgs; [
     # Flakes use Git to pull dependencies from data sources 
     git
-    (import ./rebuild.nix { inherit pkgs; })
+    (import ./bin/rebuild.nix { inherit pkgs; })
   ];
 
   environment.pathsToLink = [ "/share/zsh" ];
