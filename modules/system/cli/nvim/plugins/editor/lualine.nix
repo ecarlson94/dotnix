4c463c3e8329024d7063a3bfcae5df607c251f11
Eric Carlson <e.carlson94@gmail.com>
2024-03-10 21:52:48 -0400

    make lualine bubbly

--- modules/system/cli/nvim/plugins/editor/lualine.nix
+++ modules/system/cli/nvim/plugins/editor/lualine.nix
@@ -1,5 +1,24 @@
 {
   plugins.lualine = {
     enable = true;
+
+    componentSeparators = {
+      left = "";
+      right = "";
+    };
+
+    sectionSeparators = {
+      left = "";
+      right = "";
+    };
+
+    sections = {
+      lualine_a = [{
+        separator.left = "";
+      }];
+      lualine_z = [{
+        separator.right = "";
+      }];
+    };
   };
 }
