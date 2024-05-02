4576b8793d7bb93ff974836c6669988d54f0a05b
Eric Carlson <e.carlson94@gmail.com>
2024-05-02 19:19:42 -0400

    fix history substring search

--- modules/home/cli/zsh/default.nix
+++ modules/home/cli/zsh/default.nix
@@ -36,6 +36,8 @@ in {
 
         historySubstringSearch = {
           enable = true;
+          searchUpKey = ["^[[A" "$terminfo[kcuu1]"];
+          searchDownKey = ["^[[B" "$terminfo[kcud1]"];
         };
 
         syntaxHighlighting = {
