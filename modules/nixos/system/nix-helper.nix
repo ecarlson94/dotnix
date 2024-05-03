90eaa434333b00d897a47dfe5ca4df22ef6423dd
Eric Carlson <e.carlson94@gmail.com>
2024-05-03 01:58:33 -0400

    Keep last 10 days worth of configs

--- modules/nixos/system/nix-helper.nix
+++ modules/nixos/system/nix-helper.nix
@@ -5,7 +5,7 @@
     clean = {
       enable = true;
       dates = "weekly";
-      extraArgs = "--keep-since 4d --keep 10";
+      extraArgs = "--keep-since 10d --keep 10";
     };
 
     flake = "/home/${config.user.name}/gitrepos/dotnix";
