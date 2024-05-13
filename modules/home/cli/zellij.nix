595185a753d5f5a1451ba84a61b01bbb843bd9c0
Eric Carlson <e.carlson94@gmail.com>
2024-05-12 20:46:55 -0400

    fix mappings

--- modules/home/cli/zellij.nix
+++ modules/home/cli/zellij.nix
@@ -11,39 +11,39 @@ in {
   config = mkIf cfg.enable {
     programs.zellij = {
       enable = true;
-      catppuccin.enable = true;
 
       settings = {
+        theme = "catppuccin-mocha";
         simplified_ui = true;
         pane_frames = false;
         ui.pane_frames.rounded_corners = true;
 
         keybinds = {
           move = {
-            "bind \"Ctrl m\"" = {
+            "bind \"Ctrl l\"" = {
               SwitchToMode = "Normal";
             };
             unbind = "Ctrl h";
           };
           "shared_except \"move\" \"locked\"" = {
-            "bind \"Ctrl m\"" = {SwitchToMode = "Move";};
+            "bind \"Ctrl l\"" = {SwitchToMode = "Move";};
             unbind = "Ctrl h";
           };
 
           search = {
-            "bind \"Ctrl ,\"" = {
+            "bind \"Ctrl w\"" = {
               SwitchToMode = "Normal";
             };
             unbind = "Ctrl s";
           };
           scroll = {
-            "bind \"Ctrl ,\"" = {
+            "bind \"Ctrl w\"" = {
               SwitchToMode = "Normal";
             };
             unbind = "Ctrl s";
           };
           "shared_except \"scroll\" \"locked\"" = {
-            "bind \"Ctrl ,\"" = {SwitchToMode = "Scroll";};
+            "bind \"Ctrl w\"" = {SwitchToMode = "Scroll";};
             unbind = "Ctrl s";
           };
         };
