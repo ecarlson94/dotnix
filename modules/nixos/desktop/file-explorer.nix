c4dc0b75bcd2c5644be36f390b0b2958a8911da3
Eric Carlson <e.carlson94@gmail.com>
2024-05-14 22:52:19 -0400

    upgrade file manager functionality

--- modules/nixos/desktop/file-explorer.nix
+++ modules/nixos/desktop/file-explorer.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    xfce.thunar
-  ];
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin # Provides file context menus for archives
+      thunar-volman # Automatic management of removable drives and media
+    ];
+  };
 
-  services.gvfs.enable = true;
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
 }
