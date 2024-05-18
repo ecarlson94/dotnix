1b0e2f413ac2ee1f58981acb20abaa4cca36e3f9
Eric Carlson <e.carlson94@gmail.com>
2024-05-18 14:30:42 -0400

    simplify conform plugin

--- modules/home/cli/nixvim/plugins/editor/conform-nvim.nix
+++ modules/home/cli/nixvim/plugins/editor/conform-nvim.nix
@@ -3,42 +3,37 @@
   lib,
   config,
   ...
-}:
-with lib; let
-  cfg = config.modules.cli.nixvim;
-in {
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [shfmt prettierd];
+}: {
+  config.home.packages = lib.mkIf config.programs.nixvim.enable (with pkgs; [shfmt prettierd]);
 
-    programs.nixvim.plugins.conform-nvim = {
-      enable = true;
-      notifyOnError = false;
+  config.programs.nixvim.plugins.conform-nvim = {
+    enable = true;
+    notifyOnError = false;
 
-      formatOnSave = {
-        lspFallback = true;
-        timeoutMs = 500;
+    formatOnSave = {
+      lspFallback = true;
+      timeoutMs = 500;
+    };
+
+    formatters = {
+      shfmt = {
+        prepend_args = ["-i" "2" "-ci"];
       };
+    };
 
-      formatters = {
-        shfmt = {
-          prepend_args = ["-i" "2" "-ci"];
-        };
-      };
-
-      formattersByFt = {
-        cs = ["csharpier"];
-        css = [["prettierd" "prettier"]];
-        elixir = ["mix"];
-        html = [["prettierd" "prettier"]];
-        javascript = [["prettierd" "prettier"]];
-        javascriptreact = [["prettierd" "prettier"]];
-        markdown = [["prettierd" "prettier"]];
-        nix = [["alejandra" "nixpkgs-fmt"]];
-        typescript = [["prettierd" "prettier"]];
-        typescriptreact = [["prettierd" "prettier"]];
-        go = ["gofmt"];
-        sh = ["shfmt"];
-      };
+    formattersByFt = {
+      cs = ["csharpier"];
+      css = [["prettierd" "prettier"]];
+      elixir = ["mix"];
+      html = [["prettierd" "prettier"]];
+      javascript = [["prettierd" "prettier"]];
+      javascriptreact = [["prettierd" "prettier"]];
+      markdown = [["prettierd" "prettier"]];
+      nix = [["alejandra" "nixpkgs-fmt"]];
+      typescript = [["prettierd" "prettier"]];
+      typescriptreact = [["prettierd" "prettier"]];
+      go = ["gofmt"];
+      sh = ["shfmt"];
     };
   };
 }
