5a0dcd6f3b0d77af5af226a7233ff0517a1be203
Eric Carlson <e.carlson94@gmail.com>
2024-06-05 20:21:33 -0400

    add dualsensectl for better ps5 controller identification

--- modules/nixos/desktop/bluetooth.nix
+++ modules/nixos/desktop/bluetooth.nix
@@ -1,8 +1,11 @@
-{
+{pkgs, ...}: {
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
   };
 
   services.blueman.enable = true;
+
+  # PS5 DualSense Control
+  environment.systemPackages = [pkgs.dualsensectl];
 }
