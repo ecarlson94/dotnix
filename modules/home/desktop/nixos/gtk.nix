8aa8d06f2ba49ce7903ccd8d67b70e422c76d9c7
Eric Carlson <e.carlson94@gmail.com>
2024-06-23 22:58:57 -0400

    replace catppuccin gtk with colloid catppuccin

--- modules/home/desktop/nixos/gtk.nix
+++ modules/home/desktop/nixos/gtk.nix
@@ -1,24 +1,20 @@
-{
-  pkgs,
-  theme,
-  ...
-}: {
+{pkgs, ...}: {
   xdg.enable = true;
   gtk = {
     enable = true;
     theme = {
-      name = "Catppuccin-Mocha-Compact-Pink-Dark";
-      package = pkgs.catppuccin-gtk.override {
-        inherit (theme) variant;
-        accents = ["pink"];
-        size = "standard";
-        tweaks = ["rimless"];
+      name = "Colloid-Teal-Dark-Compact-Catppuccin";
+      package = pkgs.colloid-gtk-theme.override {
+        themeVariants = ["teal"];
+        colorVariants = ["dark"];
+        sizeVariants = ["compact"];
+        tweaks = ["catppuccin"];
       };
     };
 
     iconTheme = {
-      package = pkgs.catppuccin-papirus-folders;
-      name = "Papirus";
+      package = pkgs.colloid-icon-theme;
+      name = "Colloid-dark";
     };
   };
 }
