909feefbf05aff12d37a5b2900e19c495be209d3
Eric Carlson <e.carlson94@gmail.com>
2024-07-19 16:53:17 +0000

    fix trouble toggle commands

--- modules/home/cli/nixvim/plugins/editor/trouble.nix
+++ modules/home/cli/nixvim/plugins/editor/trouble.nix
@@ -1,43 +1,49 @@
 {
   programs.nixvim = {
-    plugins.trouble.enable = true;
+    plugins.trouble = {
+      enable = true;
+
+      settings = {
+        warn_no_results = false;
+      };
+    };
 
     keymaps = [
       {
         mode = ["n"];
         key = "<leader>xx";
-        action = "<cmd>TroubleToggle<cr>";
-        options.desc = "Toggle Trouble";
+        action = "<cmd>Trouble diagnostics toggle<cr>";
+        options.desc = "Diagnostics (Trouble)";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>xX";
+        action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+        options.desc = "Buffer Diagnostics (Trouble)";
+      }
+      {
+        mode = ["n"];
+        key = "<leader>cs";
+        action = "<cmd>Trouble symbols toggle focus=false<cr>";
+        options.desc = "Symbols (Trouble)";
       }
       {
         mode = ["n"];
         key = "<leader>xl";
-        action = "<cmd>TroubleToggle loclist<cr>";
-        options.desc = "Toggle Location List";
+        action = "<cmd>Trouble loclist toggle<cr>";
+        options.desc = "Location List (Trouble)";
       }
       {
         mode = ["n"];
         key = "<leader>xq";
-        action = "<cmd>TroubleToggle quickfix<cr>";
-        options.desc = "Toggle Quickfix List";
-      }
-      {
-        mode = ["n"];
-        key = "<leader>xd";
-        action = "<cmd>TroubleToggle document_diagnostics<cr>";
-        options.desc = "Toggle Document Diagnostics";
-      }
-      {
-        mode = ["n"];
-        key = "<leader>xw";
-        action = "<cmd>TroubleToggle workspace_diagnostics<cr>";
-        options.desc = "Toggle Workspace Diagnostics";
+        action = "<cmd>Trouble qflist toggle<cr>";
+        options.desc = "Quickfix List (Trouble)";
       }
       {
         mode = ["n"];
         key = "gR";
-        action = "<cmd>TroubleToggle lsp_references<cr>";
-        options.desc = "Toggle Quickfix List";
+        action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+        options.desc = "LSP Definitons / references / ... (Trouble)";
       }
       {
         mode = ["n"];
