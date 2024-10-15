4e56a85a567b9bd92c7e2f7b642711bbb3d7a760
Eric Carlson <ecarlson@walawren.com>
2024-10-15 17:33:27 -0400

    remove unnecessary code in user module

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,16 +1,10 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-with lib; let
-  cfg = config.user;
-
-  mkOpt = type: default: description:
-    mkOption {inherit type default description;};
-in {
-  options.user = with types; {
-    name = mkOpt str "walawren" "The name to use for the user account";
+{lib, ...}:
+with lib; {
+  options.user = {
+    name = mkOption {
+      type = types.str;
+      default = "walawren";
+      description = "The name to use for the user account";
+    };
   };
 }
