1e1857edf75e25b08f72ad8cf10754600edbd1e3
Eric Carlson <ecarlson@walawren.com>
2025-01-05 20:33:23 -0500

    rename pulseaudio

--- modules/nixos/ui/sound.nix
+++ modules/nixos/ui/sound.nix
@@ -16,18 +16,19 @@ in {
 
     programs.noisetorch.enable = true; # Mic Noise Filter
 
-    hardware.pulseaudio.support32Bit = true;
-
     security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-
-      pulse.enable = true;
-      jack.enable = true;
-      wireplumber.enable = true;
-      alsa = {
+    services = {
+      pulseaudio.support32Bit = true;
+      pipewire = {
         enable = true;
-        support32Bit = true;
+
+        pulse.enable = true;
+        jack.enable = true;
+        wireplumber.enable = true;
+        alsa = {
+          enable = true;
+          support32Bit = true;
+        };
       };
     };
   };
