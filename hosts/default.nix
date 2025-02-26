13e1dd6b0d6356c1ab2264dafea84fdc825c5e87
Eric Carlson <ecarlson@walawren.com>
2025-02-26 01:02:49 -0500

    configure ssh and fail2ban for virtualbox

--- hosts/default.nix
+++ hosts/default.nix
@@ -90,6 +90,30 @@ in
             useOSProber = true;
           };
 
+          services.openssh = {
+            enable = true;
+            ports = [22];
+            settings = {
+              PasswordAuthentication = true;
+              AllowUsers = null; # Allows all users by default. Can be [ "user1" "user2" ]
+              UseDns = true;
+              X11Forwarding = false;
+              PermitRootLogin = "no"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+            };
+          };
+
+          services.fail2ban = {
+            enable = true;
+            ignoreIP = [
+              # Whitelist some subnets
+              "10.0.0.0/8"
+              "172.16.0.0/12"
+              "192.168.0.0/16"
+              "8.8.8.8" # whitelist a specific IP
+              "nixos.wiki" # resolve the IP via DNS
+            ];
+          };
+
           user.name = "kiri";
         }
       ];
