c064b949fba485916aacae3ae8b34fd1ba320038
Kiri Carlson <kiri@walawren.com>
2025-02-28 00:51:41 -0500

    add initial configuration for disko, impermanence, and nixos-anywhere installation

--- flake.nix
+++ flake.nix
@@ -15,6 +15,15 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+
     # User configuration dependencies
     home-manager = {
       url = "github:nix-community/home-manager";
