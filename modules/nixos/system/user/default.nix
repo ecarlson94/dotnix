074b3e2c7d19e46f5a94bde21b535ebd50a566bf
Kiri Carlson <kiri@walawren.com>
2025-03-31 19:33:08 -0400

    move home folder service to separate file

--- modules/nixos/system/user/default.nix
+++ modules/nixos/system/user/default.nix
@@ -7,6 +7,8 @@
 with lib; let
   pubKeys = filesystem.listFilesRecursive ./keys;
 in {
+  imports = [./ensure-home-exists.nix];
+
   options = {
     user.name = mkOption {
       type = types.str;
@@ -53,43 +55,5 @@ in {
       fi
     '';
     programs.fish.enable = true;
-
-    systemd.services."persist-home-create-root-paths" = let
-      persistentHomesRoot = "/persist";
-
-      listOfCommands =
-        lib.mapAttrsToList
-        (
-          _: user: let
-            userHome = lib.escapeShellArg (persistentHomesRoot + user.home);
-          in ''
-            if [[ ! -d ${userHome} ]]; then
-                echo "Persistent home root folder '${userHome}' not found, creating..."
-                mkdir -p --mode=${user.homeMode} ${userHome}
-                chown ${user.name}:${user.group} ${userHome}
-            fi
-          ''
-        )
-        (lib.filterAttrs (_: user: user.createHome == true) config.users.users);
-
-      stringOfCommands = lib.concatLines listOfCommands;
-    in {
-      script = stringOfCommands;
-      unitConfig = {
-        Description = "Ensure users' home folders exist in the persistent filesystem";
-        PartOf = ["local-fs.target"];
-        # The folder creation should happen after the persistent home path is mounted.
-        After = ["persist-home.mount"];
-      };
-
-      serviceConfig = {
-        Type = "oneshot";
-        StandardOutput = "journal";
-        StandardError = "journal";
-      };
-
-      # [Install]
-      wantedBy = ["local-fs.target"];
-    };
   };
 }
