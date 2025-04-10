df59a8bd9ee9251ad1bb3111076c2843119c0b75
Kiri Carlson <kiri@walawren.com>
2025-04-10 13:28:40 -0400

    fix file name of hardware-configuration creation

--- scripts/bootstrap-nixos.sh
+++ scripts/bootstrap-nixos.sh
@@ -145,7 +145,7 @@ function nixos_anywhere() {
     green "Generating hardware-configuration.nix on $target_hostname and adding it to the local dotnix."
     $ssh_root_cmd "nixos-generate-config --no-filesystems --root /mnt"
     $scp_cmd root@"$target_destination":/mnt/etc/nixos/hardware-configuration.nix \
-      "${git_root}"/hosts/hardware/nixos-"$target_hostname".nix
+      "${git_root}"/hosts/hardware/"$target_hostname".nix
     generated_hardware_config=1
   fi
 
