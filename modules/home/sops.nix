ee19ea66474edae7ba377e297c1bc81f32d8f983
Kiri Carlson <kiri@walawren.com>
2025-05-17 05:14:30 -0400

    fix ssh key permissions

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -1,6 +1,8 @@
 {
   hostConfig,
   inputs,
+  lib,
+  pkgs,
   ...
 }: {
   imports = [inputs.sops-nix.homeManagerModules.sops];
@@ -11,9 +13,7 @@
 
     age = {
       # automatcally import host SSH keys as age keys
-      sshKeyPaths = [
-        "/home/${hostConfig.user.name}/.ssh/${hostConfig.user.name}_ssh_key"
-      ];
+      sshKeyPaths = ["/home/${hostConfig.user.name}/.ssh/${hostConfig.user.name}_ssh_key"];
 
       keyFile = "/home/${hostConfig.user.name}/.config/sops/age/keys.txt";
 
@@ -21,4 +21,12 @@
       generateKey = true;
     };
   };
+
+  # I need this because `sops.age.generateKey` is not working
+  home.activation.ensureAgeKeyDir = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
+    mkdir -p ~/.config/sops/age
+    chmod 700 ~/.config/sops
+    chmod 700 ~/.config/sops/age
+    ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i ~/.ssh/${hostConfig.user.name}_ssh_key > ~/.config/sops/age/keys.txt
+  '';
 }
