0df4d43f91d4930d64faae7e6956b0cca20793d9
Kiri Carlson <kiri@walawren.com>
2025-05-17 06:19:13 -0400

    add lock on lid close

--- modules/home/ui/nixos/hyprland/hyprlock.nix
+++ modules/home/ui/nixos/hyprland/hyprlock.nix
@@ -82,5 +82,30 @@ in {
         (mkIf fingerprint.enable "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1")
       ];
     };
+
+    systemd.user.services.hyprlock-before-suspend = {
+      Unit = {
+        Description = "Lock with hyprlock before system suspend";
+        PartOf = ["graphical-session.target"];
+        After = ["graphical-session.target"];
+      };
+
+      Install = {
+        WantedBy = ["graphical-session.target"];
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = pkgs.writeShellScript "hyprlock-dbus-suspend-watcher" ''
+          ${pkgs.dbus}/bin/dbus-monitor --system "type='signal',interface='org.freedesktop.login1.Manager',member='PrepareForSleep'" |
+          while read -r line; do
+            if echo "$line" | grep -q "true"; then
+              ${pkgs.hyprlock}/bin/hyprlock
+            fi
+          done
+        '';
+        Restart = "always";
+      };
+    };
   };
 }
